fd7751cf3e7a3273bdaf0a2ba0c1916660d761f0
quantium-rock <alexcollsoutumuro@gmail.com>
2022-01-17 14:11:59 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ config/index.js
@@ -0,0 +1,9 @@
+export const config = {
+  env: "prod",
+  LOCAL_API_URL: "localhost:3000",
+  API_URL: "odp-rarity.vercel.app",
+  COLLECTION_NAME: "OneDayPunks",
+  COLLECTION_TITLE: "One Day Punks",
+  COLLECTION_DESCRIPTION: '10k "One Day I\'ll Be A Punk"-punks.',
+  COLLECTION_IMG_LINK: "https://punkscape.xyz/og.png",
+};
